f7538a9aa6cb5efbcef957c78e65472136ba21f5
Harivansh <hello@harivan.sh>
2022-06-17 20:38:58 +0400

    feat: add simple controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ChatGateway } from './chat.gateway';
+import { AppController } from './app.controller';
 
 @Module({
   providers: [ChatGateway],
+  controllers: [AppController],
 })
 export class AppModule {}
