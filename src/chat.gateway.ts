4291b6320a903e835de67baa06a7eeb3353b0877
Harivansh <hello@harivan.sh>
2022-06-17 15:17:15 +0400

    feat: setup chat gateway

--- src/chat.gateway.ts
+++ src/chat.gateway.ts
@@ -1,9 +1,18 @@
-import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import {
+  MessageBody,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { Server } from 'socket.io';
 
-@WebSocketGateway()
+@WebSocketGateway({ cors: true })
 export class ChatGateway {
-  @SubscribeMessage('message')
-  handleMessage(client: any, payload: any): string {
-    return 'Hello world!';
+  @WebSocketServer()
+  private readonly server: Server;
+
+  @SubscribeMessage('broadcast')
+  handleBroadcast(@MessageBody() message: string): void {
+    this.server.emit('broadcast', message);
   }
 }
